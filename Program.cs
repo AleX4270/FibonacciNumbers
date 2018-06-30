f7f78315706660bbd1c4cab52abd80c8592c447b
AleX <alexofficial@vp.pl>
2018-06-30 18:00:26 +0200

    added comments

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
+
+//Made by AleX4270(2018)
+//Feel free to use :)
 
 namespace FibonacciNumbers
 {
@@ -14,14 +13,14 @@ namespace FibonacciNumbers
             Console.WriteLine("Welcome! This program will write as much Fibonacci numbers as you want :)");
 
             
-            ulong[] fib = new ulong[10000];
-            fib[1] = 1;
+            ulong[] fib = new ulong[10000]; //This array contains our Fibonacci numbers.
+            fib[1] = 1; //First fibonacci number is 0 so second has to be 1 :)
 
-            int howMuch;
+            int howMuch; //This variable is used to contain information about how much numbers program should print.
 
             input:
             Console.WriteLine("How much Fibonacci numbers do you want to see?: ");
-            try
+            try //Simple try and catch :) In case someone will type something else instead of a number.
             {
                 howMuch = int.Parse(Console.ReadLine());
             }
@@ -37,17 +36,17 @@ namespace FibonacciNumbers
             if(howMuch > 100)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Keep in mind that your computer may not be able to write out such large numbers!");
-                Console.ForegroundColor = ConsoleColor.White;
-                Thread.Sleep(5000);
+                Console.WriteLine("Keep in mind that your computer may not be able to write out such large numbers!"); //Simple warning message
+                Console.ForegroundColor = ConsoleColor.White;                                             
+                Thread.Sleep(5000); 
 
 
             }
 
             Console.WriteLine("The value of the {0} Fibonacci number is: {1}", 1, fib[1]);
-            try
+            try //Another try and catch in case someone will type numbers above 10000.
             {
-                for (int i = 2; i <= howMuch; i++)
+                for (int i = 2; i <= howMuch; i++) //For loop that prints every fibonacci number on the screen.
                 {
                     fib[i] = fib[i - 2] + fib[i - 1];
                     Console.WriteLine("The value of the {0} Fibonacci number is: {1}", i, fib[i]);
